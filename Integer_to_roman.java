f7e0dc4849b1dac4af39121e52edafa105bb06f1
Mohasin Patel <mohasinpatel313@gmail.com>
2023-02-19 23:48:51 +0530

    Update Integer_to_roman.java

--- Integer_to_roman.java
+++ Integer_to_roman.java
@@ -81,3 +81,27 @@ class Solution {
 
     }
 }
+
+
+// another method slower
+class Solution2 {
+    public String intToRoman(int num) 
+    {
+        int n[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String r[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        
+        StringBuilder str = new StringBuilder();
+        
+        for(int i = 0; i<n.length; i++)
+        {
+            while(num>=n[i])
+            {
+                str.append(r[i]);
+                num = num - n[i];
+            }
+        }
+
+        return str.toString();
+
+    }
+}
