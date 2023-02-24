bf17d0467f243282b9341c10590fc1ceb1e1ea23
Mohasin Patel <mohasinpatel313@gmail.com>
2023-02-24 23:54:20 +0530

    Add files via upload

---
+++ search_insert_index.java
@@ -0,0 +1,54 @@
+/* 
+problem statement :
+Given a sorted array of distinct integers and a target value, return the index if the 
+target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+*/
+
+
+public class search_insert_index 
+{
+    public static void main(String[] args) 
+    {
+        int nums[] = {1,3,5,6};
+
+        Solution sl = new Solution();
+        
+        System.out.println(sl.searchInsert(nums, 5));
+    }    
+}
+
+class Solution 
+{
+    public int searchInsert(int[] nums, int target) 
+    {   
+        for(int i = 0; i<nums.length; i++)    
+        {
+            if(target < nums[i] || target==nums[i])
+            {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
