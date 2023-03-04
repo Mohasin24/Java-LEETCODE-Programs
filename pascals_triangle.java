e7d581dc5a072b0cf06333467762d2e57864c376
Mohasin Patel <mohasinpatel313@gmail.com>
2023-03-05 00:37:57 +0530

    Update pascals_triangle.java

--- pascals_triangle.java
+++ pascals_triangle.java
@@ -1,5 +1,28 @@
 /*
+Problem Statement :
+Given an integer numRows, return the first numRows of Pascal's triangle.
 
+        1 
+       1 1                 
+      1 2 1                
+     1 3 3 1               
+    1 4 6 4 1
+    
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+Example 1:
+
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+Example 2:
+
+Input: numRows = 1
+Output: [[1]]
+ 
+
+Constraints:
+
+1 <= numRows <= 30
 */
 
 import java.lang.*;
@@ -36,10 +59,11 @@ class Solution
                 }
                 else
                 {
+                    // accessing previous row elements
                     row.add(matrix.get(i-1).get(j-1)+matrix.get(i-1).get(j));
                 }
             }
-
+        
             matrix.add(row);
         }
 
