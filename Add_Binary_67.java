09abb8da716bcc9c1f2af4a72b455125aa778c59
Mohasin Patel <mohasinpatel313@gmail.com>
2023-05-25 14:34:01 +0530

    Update Add_Binary_67.java

--- Add_Binary_67.java
+++ Add_Binary_67.java
@@ -7,7 +7,7 @@ Given two binary strings a and b, return their sum as a binary string.
 Example 1:
 
 Input: a = "11", b = "1"
-Output: "100"
+Output: "100"               
 Example 2:
 
 Input: a = "1010", b = "1011"
