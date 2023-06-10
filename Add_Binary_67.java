cd09b26d067f5ff3f837dce6f44a5cae9235f650
Mohasin Patel <mohasinpatel313@gmail.com>
2023-06-10 21:58:22 +0530

    Update Add_Binary_67.java

--- Add_Binary_67.java
+++ Add_Binary_67.java
@@ -10,7 +10,7 @@ Input: a = "11", b = "1"
 Output: "100"               
 Example 2:
 
-Input: a = "1010", b = "1011"
+Input: a = "1010", b = "1011"                                  
 Output: "10101"
  
 
