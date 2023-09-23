e19ab1ca1bd0d589266dfdf467730d38a2cada27
Mohasin Patel <mohasinpatel313@gmail.com>
2023-09-23 20:11:24 +0530

    Update Add_to_Array_Form_of_Integer_989.java

--- Add_to_Array_Form_of_Integer_989.java
+++ Add_to_Array_Form_of_Integer_989.java
@@ -5,10 +5,10 @@ The array-form of an integer num is an array representing its digits in left to 
 For example, for num = 1321, the array form is [1,3,2,1].                                     
 Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.               
               
-                                                          
+                                                           
 
 Example 1:                     
-
+    
 Input: num = [1,2,0,0], k = 34
 Output: [1,2,3,4]
 Explanation: 1200 + 34 = 1234
