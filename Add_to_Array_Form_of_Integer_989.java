ab9a69b7d09b14600495ad5e3c0548defaa1fc1e
Mohasin Patel <mohasinpatel313@gmail.com>
2023-12-24 23:36:56 +0530

    Update Add_to_Array_Form_of_Integer_989.java

--- Add_to_Array_Form_of_Integer_989.java
+++ Add_to_Array_Form_of_Integer_989.java
@@ -12,7 +12,7 @@ Example 1:
 Input: num = [1,2,0,0], k = 34
 Output: [1,2,3,4]                                             
 Explanation: 1200 + 34 = 1234
-Example 2:
+Example 2:          
 
 Input: num = [2,7,4], k = 181
 Output: [4,5,5]
